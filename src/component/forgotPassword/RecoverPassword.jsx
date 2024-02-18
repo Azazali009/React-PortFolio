b96aea0e88fe8ade66573d171d639382a771f17d
Azaz ali <aizaz.0938@gmail.com>
2024-02-18 13:14:58 +0500

    clean code

--- src/component/forgotPassword/RecoverPassword.jsx
+++ src/component/forgotPassword/RecoverPassword.jsx
@@ -20,20 +20,20 @@ const RecoverPassword = () => {
       {
         onSuccess: () => {
           toast.success("Password reset successfully.");
-          navigate("/login");
+          navigate("/");
         },
         onError: (err) => toast.error(err.message),
-      }
+      },
     );
   };
   const onError = (err) => {};
   return (
-    <div className="min-h-screen w-full flex items-center justify-center dark:bg-bodyColor bg-gray-200 font-primary text-gray-500 dark:text-white">
-      <div className="dark:shadow-shadowOne shadow-shadowTwo p-6 rounded-xl">
-        <h2 className=" text-2xl font-medium mb-2 text-gray-600 dark:text-white ">
+    <div className="flex min-h-screen w-full items-center justify-center bg-gray-200 font-primary text-gray-500 dark:bg-bodyColor dark:text-white">
+      <div className="rounded-xl p-6 shadow-shadowTwo dark:shadow-shadowOne">
+        <h2 className=" mb-2 text-2xl font-medium text-gray-600 dark:text-white ">
           Reset your password
         </h2>
-        <p className=" text-sm w-3/4 dark:text-gray-400 text-gray-500 font-semibold mb-8">
+        <p className=" mb-8 w-3/4 text-sm font-semibold text-gray-500 dark:text-gray-400">
           Type in a new secure password and press save to update your password
         </p>
 
@@ -41,14 +41,14 @@ const RecoverPassword = () => {
           onSubmit={handleSubmit(onSubmit, onError)}
           className=" flex flex-col gap-2"
         >
-          <label className="dark:text-gray-400 text-gray-600  font-medium capitalize">
+          <label className="font-medium capitalize  text-gray-600 dark:text-gray-400">
             new password
           </label>
           <div className="flex flex-col gap-1">
             <input
               type="password"
               disabled={isUpdating}
-              className=" input input-bordered input-info dark:text-slate-50 text-slate-700  dark:bg-transparent bg-gray-200 input-sm"
+              className=" input-bordered input-info input input-sm bg-gray-200  text-slate-700 dark:bg-transparent dark:text-slate-50"
               {...register("password", {
                 required: "This field is required",
                 minLength: {
@@ -64,16 +64,16 @@ const RecoverPassword = () => {
           </div>
           <button
             disabled={isUpdating}
-            className=" btn btn-sm btn-success capitalize hover:bg-opacity-70 duration-200"
+            className=" btn-success btn-sm btn capitalize duration-200 hover:bg-opacity-70"
           >
             save new password
           </button>
-          <p className=" text-center mt-4 text-sm text-gray-600 font-medium dark:text-gray-300">
+          <p className=" mt-4 text-center text-sm font-medium text-gray-600 dark:text-gray-300">
             already have an account?
             <Link
               disabled={isUpdating}
               to={"/login"}
-              className=" link capitalize link-info"
+              className=" link-info link capitalize"
             >
               sign in
             </Link>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LoginForm from "../component/login/LoginForm";
+import { useCurrentUser } from "../component/authentication/useCurrentUser";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const { isAuthenticated } = useCurrentUser();
+
+  useEffect(() => {
+    if (isAuthenticated) return navigate("/");
+  }, [navigate, isAuthenticated]);
   return (
     <div className=" flex min-h-screen justify-center bg-gray-200 font-primary dark:bg-bodyColor">
       <LoginForm />
