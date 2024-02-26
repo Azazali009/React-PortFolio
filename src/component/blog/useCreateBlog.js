157f597f00a6bc2fdfd740d738f87f5143ed4d0c
Azaz ali <aizaz.0938@gmail.com>
2024-02-26 09:12:50 +0500

    Add two  blogs about html and css

--- src/component/blog/useCreateBlog.js
+++ src/component/blog/useCreateBlog.js
@@ -1,12 +1,18 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createBlog } from "../../services/apiBlog";
+import toast from "react-hot-toast";
 
 export function useCreateBlog() {
   const queryClient = useQueryClient();
   const { mutate: addBlog, isPending } = useMutation({
     mutationFn: createBlog,
-    onSuccess: () => alert("Created"),
-    onError: () => alert("Error"),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["Blogs"] });
+      toast.success("Blog created successfully/");
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
   });
   return { addBlog, isPending };
 }
